73c6c35fd051699f24fbcf35a3c2fb59a795e18e
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2014-09-07 20:16:32 +0300

    Getting temperature values via HTTP from DS18x20 sensors

---
+++ server.js
@@ -0,0 +1,38 @@
+"use strict";
+var fs = require("fs");
+var nconf = require("nconf");
+var express = require('express');
+var sensor = require('ds18x20');
+var app = express();
+
+var isLoaded = sensor.isDriverLoaded();
+console.log("Is sensor driver loaded: " + isLoaded);
+
+nconf.argv()
+ .env()
+ .file({ file: "./config.json" });
+
+var sensors = nconf.get("sensors");
+var port = nconf.get("port");
+
+app.get('/temperature/:name', function (req, res) {
+  var name = req.params.name;
+  var id = sensors[name];
+
+  if (id === undefined) {
+    res.status(404).send("Sensor name " + name + " not found");
+    return;
+  }
+
+  sensor.get(id, function (err, temp) {
+    if (temp === undefined) {
+      console.log(err);
+      res.status(500).send("Error: " + err);
+    } else {
+      res.send(temp);
+    }
+  });
+});
+
+console.log("Server started at http://127.0.0.1:" + port);
+app.listen(port);
